de88f6bb55b577d8bc60172ec19c799926175093
Brenno Araújo <brennoaraujo@yahoo.com>
2022-09-15 15:18:22 -0300

    chore

--- 17_const.js
+++ 17_const.js
@@ -1,24 +1,24 @@
-// function getHora(hora){
-//     if (hora > 12 && hora <= 18) {
-//         return 'Boa tarde'
-//     }
+function getHora(hora){
+    if (hora > 12 && hora <= 18) {
+        return 'Boa tarde'
+    }
 
-//     if (hora > 18 && hora <= 24) {
-//         return 'Boa noite'
-//     }
+    if (hora > 18 && hora <= 24) {
+        return 'Boa noite'
+    }
 
-//     if (hora > 0 && hora <= 6) {
-//         return 'Boa madrugada'
-//     }
+    if (hora > 0 && hora <= 6) {
+        return 'Boa madrugada'
+    }
 
-//     return 'Bom dia'
-// }
+    return 'Bom dia'
+}
 
-// function saudacoes(nome, hora) {
-//     return `${getHora(hora)} ${nome}`
-// }
+function saudacoes(nome, hora) {
+    return `${getHora(hora)} ${nome}`
+}
 
-// console.log(saudacoes('João', 7))
+console.log(saudacoes('João', 7))
 
 const usuario = {
     nome: 'João',
--- 25_ternarios.js
+++ 25_ternarios.js
@@ -1,5 +1,4 @@
-const idade = 26;
+const idade = 15;
 
 idade == 25 ? console.log('É 25') : console.log('Não é 25');
-
 
