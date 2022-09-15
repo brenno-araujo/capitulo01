4183309361e087e2f2c77623e6280b29e80e99bf
Brenno Araújo <brennoaraujo@yahoo.com>
2022-09-15 12:53:42 -0300

    chore

--- 15_imutabilidade.js
+++ 15_imutabilidade.js
@@ -1,13 +1,30 @@
-const casa1 = {
-    cor: 'azul',
-    localizacao: 'rua 1',
-    numero: 1
-}
-const casa2 = { 
-    ...casa1,
-    numero: 2
-}
+// const casa1 = {
+//     cor: 'azul',
+//     localizacao: 'rua 1',
+//     numero: 1
+// }
+// const casa2 = { 
+//     ...casa1,
+//     numero: 2,
+//     complemento: 'casa 2'
+// }
 
 
-console.log(casa1)
-console.log(casa2)
+// console.log(casa1)
+// console.log(casa2)
+
+const joao = {
+    enderecos: [
+        {rua: 'rua 1', numero: 1},
+    ]
+}
+
+const maria = {
+    ...joao,
+    ...joao.enderecos,
+}
+
+maria.enderecos.push({rua: 'rua 3', numero: 3})
+
+console.log(joao)
+console.log(maria)
